6dce190bab41410d45a80d0de14b0f71ffd40647
Pujith Soma <somapujith@gmail.com>
2026-07-09 04:46:58 +0530

    feat: add AI orb demo page

---
+++ frontend/src/pages/AIOrbDemo.jsx
@@ -0,0 +1,9 @@
+import AIOrb from '../components/AIOrb';
+
+export default function AIOrbDemo() {
+  return (
+    <div className="fixed inset-0 flex items-center justify-center bg-white">
+      <AIOrb size={360} />
+    </div>
+  );
+}
